9f04a26a9b3d1f444390b04b4b3dc364207d57ce
Denis Klyavin <denisklyavin@MacBook-Pro-Denis.local>
2024-05-30 08:36:14 +0300

    фикс дз2

--- Lesson_2/lesson_2_task_10.py
+++ Lesson_2/lesson_2_task_10.py
@@ -2,7 +2,10 @@ def bank(x, y):
     rate = 0.10
     sum_after_y_years = x * (1 + rate) ** y
     return sum_after_y_years
+
+
 x = float(input("Какую сумму вы хотите внести на депозит? "))
 y = int(input("На сколько лет вы хотели бы сохранить свои инвестиции? "))
 result = bank(x, y)
-print(f"Если вы вносите депозит $ {x} на {y} лет, то у вас будет $ {result:.2f} на вашем счету")
+print(f"Если вы вносите депозит $ {x} на {
+      y} лет, то у вас будет $ {result:.2f} на вашем счету")
